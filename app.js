296b77385d0a94a2cd076968257dfdd39beded9c
Michael Huang <michael.s.huang.01@gmail.com>
2023-11-25 22:03:05 -0500

    users and db added per a5 and a6 instructions

---
+++ app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import "dotenv/config";
+import session from 'express-session';
+import UserRoutes from './users/routes.js';
+
+const CONNECTION_STRING = "mongodb://127.0.0.1:27017/webdevproject";
+// this assumes mongodb compass local connection to a db called webdevproject
+// for now use the supplied data from A5/A6
+
+mongoose.connect(CONNECTION_STRING);
+const app = express();
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    })
+);
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+
+app.use(session(sessionOptions));
+app.use(express.json());
+UserRoutes(app);
+app.get('/hello', (req, res) => {res.send('Life is good!')})
+app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+app.listen(4000);
